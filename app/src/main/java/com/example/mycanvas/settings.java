580493fba7630026fcfd102a353fe014e077df28
aakash0210 <aakashgopal210@gmail.com>
2021-11-24 09:38:16 +0530

    setting xml update

--- app/src/main/java/com/example/mycanvas/settings.java
+++ app/src/main/java/com/example/mycanvas/settings.java
@@ -8,10 +8,18 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class settings extends AppCompatActivity implements View.OnClickListener {
+
+    TextView userinfo;
+
+
     FirebaseServices firebaseServices = new FirebaseServices();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
